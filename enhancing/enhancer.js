e0afcbab66ee99a257010d2a517939116ddb4ec5
Natalia <nataliamirandac@hotmail.com>
2020-08-19 17:02:17 -0400

    repair() test - passed

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -13,10 +13,16 @@ function fail(item) {
   return { ...item };
 }
 
-function repair(item) {
-  return { ...item };
+function repair(a, b) {
+  return a + b;
 }
 
 function get(item) {
   return { ...item };
 }
+
+//a repair(item) method that accepts an item object and returns a new item with the durability restored to 100. This method is the simplest of the three and would be a good starting point on this project.
+
+// a success(item) method that accepts an item object and returns a new item object modified according to the rules defined by the client for enhancement success.
+
+// a fail(item) method that accepts an item object and returns a new item object modified according to the rules defined by the client for enhancement failure.
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,9 @@
-const enhancer = require('./enhancer.js');
+const { success, fail, repair } = require("./enhancer.js");
 // test away!
+
+//----- repair() test
+describe("repair()", () => {
+  it("returns a new item with the durability restored to 100", () => {
+    expect(repair(50, 50)).toBe(100);
+  });
+});
